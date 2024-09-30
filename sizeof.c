47d8b841fa16e700eb6cf1178326a4f0c806b115
Santhini16 <santhinimuthu16@gmail.com>
2024-09-30 19:36:45 +0530

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+  int a;
+  float b;
+  char c;
+  double d;
+  printf("the size of int:%d bytes\n",sizeof(a));
+  printf("the size of float :%d bytes\n",sizeof(b));
+  printf("the size of char:%d bytes\n",sizeof(c));
+  printf("te size of double:%d bytes\n",sizeof(d));
+}
